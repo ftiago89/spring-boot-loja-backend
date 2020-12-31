fbc59da2502c81d06b961f79ac79885051903ea7
Felipe Melo <ftiagomelo89@gmail.com>
2020-12-31 09:57:04 -0300

    Atualizacao: utilizando soment JsonIgnore para tratar referencia ciclica

--- src/main/java/com/felipemelo/lojabackend/domain/Endereco.java
+++ src/main/java/com/felipemelo/lojabackend/domain/Endereco.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco {
@@ -21,7 +21,7 @@ public class Endereco {
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
