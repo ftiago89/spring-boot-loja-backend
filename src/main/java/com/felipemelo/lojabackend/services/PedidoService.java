75362c1fba0ee2dae6e2bead7309114cbf7b8a3b
Felipe Melo <ftiagomelo89@gmail.com>
2020-12-31 09:46:45 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/felipemelo/lojabackend/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.felipemelo.lojabackend.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipemelo.lojabackend.domain.Pedido;
+import com.felipemelo.lojabackend.repositories.PedidoRepository;
+import com.felipemelo.lojabackend.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
