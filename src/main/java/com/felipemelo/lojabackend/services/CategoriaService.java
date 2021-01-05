58bd8448a322223257a93296e26e850a2ff395fb
Felipe Melo <ftiagomelo89@gmail.com>
2021-01-05 10:08:38 -0300

    Adicionado DELETE para Categoria

--- src/main/java/com/felipemelo/lojabackend/services/CategoriaService.java
+++ src/main/java/com/felipemelo/lojabackend/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.felipemelo.lojabackend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.felipemelo.lojabackend.domain.Categoria;
 import com.felipemelo.lojabackend.repositories.CategoriaRepository;
+import com.felipemelo.lojabackend.services.exceptions.DataIntegrityException;
 import com.felipemelo.lojabackend.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Uma categoria não pode ser excluída quando possui produtos");
+		}
 	}
 
 }
