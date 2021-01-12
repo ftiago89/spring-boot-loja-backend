0b5ebd68c747dced59442511af4be6e5f96ee32c
Felipe Melo <ftiagomelo89@gmail.com>
2021-01-12 10:09:11 -0300

    Validacao customizada: CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/felipemelo/lojabackend/dto/ClienteNewDTO.java
+++ src/main/java/com/felipemelo/lojabackend/dto/ClienteNewDTO.java
@@ -2,23 +2,46 @@ package com.felipemelo.lojabackend.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.felipemelo.lojabackend.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Size(min = 5, max = 120, message = "Precisa ter no mínimo 5 e no máximo 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "E-mail inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
-	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+	
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
 	
 	private Integer cidadeId;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
