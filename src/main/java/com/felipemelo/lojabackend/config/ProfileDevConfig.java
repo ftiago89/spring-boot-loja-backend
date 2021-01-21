ff623f003d0b4df941518eea5e9a4603091f9cd1
Felipe Melo <ftiagomelo89@gmail.com>
2021-01-21 10:02:32 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/felipemelo/lojabackend/config/ProfileDevConfig.java
@@ -0,0 +1,33 @@
+package com.felipemelo.lojabackend.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.felipemelo.lojabackend.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class ProfileDevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateTest() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
